f1ef4203190988db92372b7158606c465f174536
Kenneth Rodriguez <kennethrdz@hotmail.com>
2023-05-22 13:22:59 -0600

    Rename firebase config variable to fix bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true, 
-  /*config: {
+  firebaseConfig: {
     apiKey: "AIzaSyCd22xswvYz4Cd_RcwJpq_in0tCIElthJQ",
     authDomain: "novedades-katy.firebaseapp.com",
     databaseURL: "https://novedades-katy-default-rtdb.firebaseio.com",
@@ -9,5 +9,5 @@ export const environment = {
     messagingSenderId: "874230415422",
     appId: "1:874230415422:web:80aad861d2406fbdcc516f",
     measurementId: "G-2D9B8422CD"
-  }*/
+  }
 };
