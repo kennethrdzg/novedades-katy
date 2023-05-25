f38eeb2ffeedfe5623817b707f2aeef5dee7a98c
Kenneth Rodriguez <kennethrdz@hotmail.com>
2023-05-24 19:56:25 -0600

    Finish setting up user creation

--- src/app/admin-usuarios/admin-usuarios.page.ts
+++ src/app/admin-usuarios/admin-usuarios.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../firebase.service';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-usuarios',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminUsuariosPage implements OnInit {
 
-  constructor() { }
+  userForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]], 
+    password: ['', [Validators.required, Validators.minLength(8)]], 
+    role: ['', [Validators.required]]
+  })
+  constructor(
+    private firebase: FirebaseService, 
+    protected router: Router, 
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    if(!this.firebase.admin){
+      this.router.navigate(['home']);
+    }
   }
 
+  crearUsuario(){
+    this.firebase.crearUsuario(this.email?.getRawValue(), this.password?.getRawValue(), this.role?.getRawValue());
+  }
+
+  get email(){
+    return this.userForm.get('email');
+  }
+
+  get password(){
+    return this.userForm.get('password');
+  }
+
+  get role(){
+    return this.userForm.get('role');
+  }
 }
