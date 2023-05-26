88e0e55b1d90045b498e41060e7c1058da316bae
Kenneth Rodriguez <kennethrdz@hotmail.com>
2023-05-25 20:43:49 -0600

    Add support for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'novedades.kay.biz.app',
   appName: 'novedades-katy',
   webDir: 'www',
   server: {
